f36f7e2ade9ea16c3e1639f22733a0b62482d668
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-05-23 07:45:49 +0700

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    // This is not present inside the default configuration
+    // but it's good to build your production application
+    // Read more about this here: https://tailwindcss.com/docs/installation#building-your-css
+    "./pages/**/*.js",
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
